863e9173001c013c041e25500ca7dc63f4921288
treeman4862 <103896800+treeman4862@users.noreply.github.com>
2025-02-12 14:06:09 +1100

    add pop

--- code/icecreamsort.py
+++ code/icecreamsort.py
@@ -11,7 +11,7 @@ class Icecream():
         if self.__pointer < self.__maxCapacity: #range check to see if icecream is full
             self.__array[self.__pointer] = flavour #if not add flavour to where pointer is 0,1,2,3,4
             self.__pointer = self.__pointer + 1 #increment pointer value
-            print(flavour + "is added!")
+            print(flavour + "  is added!")
         else:
             print("No more space!") #nothing is done user is told there is no space
     def pop(self): # eating top piece of icecream
@@ -25,4 +25,6 @@ class Icecream():
 
 myIcecream = Icecream() #creates instance of class, object
 
-myIcecream.push(fortnite)
+myIcecream.push("fortnite")
+
+print("ate " + myIcecream.pop())
